8af38a8735d6d9f5ea05b76f8b654c58daf83b7f
Alexandre Claveau <a.claveau29@gmail.com>
2021-03-09 15:56:32 +0100

    Match with sad words in the channel to interact with them

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const Discord = require("discord.js");
 const fetch = require("node-fetch");
 
+const sadWords = ["sad", "depressed", "unhappy", "angry"];
+
+const encouragements = [
+    "Cheer up!",
+    "Hang in there.",
+    "You are a great person / bot!"
+];
+
 function getQuote(){
     return fetch("https://zenquotes.io/api/random")
         .then(res => {
@@ -24,6 +32,11 @@ client.on("message", msg => {
         getQuote().then(quote =>{
             msg.channel.send(quote);
         });
+    }
+
+    if (sadWords.some(word => msg.content.includes(word))) {
+        const encouragement = encouragements[Math.floor(Math.random()*encouragements.length)];
+        msg.reply(encouragement);
     }
 });
 // process.env.TOKEN
